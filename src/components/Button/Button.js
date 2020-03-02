012b772f1eb6b4b3c6208f8854eb222e507bfabf
vebradev <justinas@vebra.dev>
2020-03-02 18:49:37 +0000

    change Button styling

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,12 +4,19 @@ import { color } from "../styles";
 
 const Button = styled(Link)`
     background-color: ${color.primary};
-    color: #ffffff;
+    color: ${color.white};
     font-size: 20px;
-    width: max-content;
+    text-align: center;
     padding: 15px 20px;
     border-radius: 4px;
     text-decoration: none;
+    border: 1px solid ${color.primary_shadow};
+    transition: all 0.2s;
+
+    &:hover {
+        cursor: pointer;
+        background-color: ${color.primary_shadow};
+    }
 `;
 
 export default Button;
