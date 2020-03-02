4c57e32dacb3577ca0eb906a5c505df71789a00f
vebradev <justinas@vebra.dev>
2020-03-02 11:32:37 +0000

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      Welcome to Dungeon
-    </>
+    <Switch>
+      <Route path="/register">Register</Route>
+      <Route path="/login">Login</Route>
+      <Route path="/play">Play</Route>
+      <Route path="/">Home</Route>
+    </Switch>
   );
 }
 
