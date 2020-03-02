a7971746d8d8356bcb14e61d38b039559dc1cbf3
vebradev <justinas@vebra.dev>
2020-03-02 13:20:35 +0000

    add Home view

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom'
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import Home from "./views/Home";
 
 function App() {
-  return (
-    <Switch>
-      <Route path="/register">Register</Route>
-      <Route path="/login">Login</Route>
-      <Route path="/play">Play</Route>
-      <Route path="/">Home</Route>
-    </Switch>
-  );
+    return (
+        <Switch>
+            <Route path="/register">Register</Route>
+            <Route path="/login">Login</Route>
+            <Route path="/play">Play</Route>
+            <Route path="/">
+                <Home />
+            </Route>
+        </Switch>
+    );
 }
 
 export default App;
